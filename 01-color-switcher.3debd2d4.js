5c7c0f414a4f43a9656fd1e9641d0546008237e4
VitaliySaburdo <107457394+VitaliySaburdo@users.noreply.github.com>
2022-11-09 06:27:47 +0000

    Deploying to gh-pages from @ VitaliySaburdo/goit-js-hw-09@5b7ee1d45bf5e2626b8054d82f63875f856434e2 🚀

---
+++ 01-color-switcher.3debd2d4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));let o=null;
+//# sourceMappingURL=01-color-switcher.3debd2d4.js.map
