a277b21f00566678a091882c4e516a56d5c61c14
VitaliySaburdo <vsaburdo@gmail.com>
2022-11-09 17:19:59 +0200

    task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,17 @@ import flatpickr from 'flatpickr';
 // Datetime picker styles
 import 'flatpickr/dist/flatpickr.min.css';
 
+const refs = {
+  inputDate: document.querySelector('input#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.addEventListener('click', () => timer.start());
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -10,4 +21,45 @@ const options = {
   minuteIncrement: 1,
 };
 
-flatpickr('#datetime-picker', options);
+flatpickr(refs.inputDate, options);
+
+const timer = {
+  start() {
+    const startTime = Date.now();
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const { days, hours, minutes, seconds } = convertMs(deltaTime);
+      upDateTimer({ days, hours, minutes, seconds });
+      refs.startBtn.setAttribute('disabled', true);
+    }, 1000);
+  },
+};
+
+function upDateTimer({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
