5b7ee1d45bf5e2626b8054d82f63875f856434e2
VitaliySaburdo <vsaburdo@gmail.com>
2022-11-09 08:27:12 +0200

    finish task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', onStartBtn);
+stoptBtn.addEventListener('click', onStopBtn);
+
+let timerId = null;
+
+function onStartBtn() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stoptBtn.removeAttribute('disabled');
+}
+
+function onStopBtn() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stoptBtn.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
