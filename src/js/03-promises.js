678fd608cb467ab69758ff15b16218b25a556534
VitaliySaburdo <vsaburdo@gmail.com>
2022-11-11 17:14:07 +0200

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,31 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  fistDelay: document.querySelector('input[name=delay]'),
+  delayStep: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+  submitBtn: document.querySelector('button'),
+};
+
+refs.submitBtn.addEventListener('click', onSubmitBtn);
+
+function onSubmitBtn(evt) {
+  evt.preventDefault();
+  const delay = refs.fistDelay.value;
+  const step = refs.delayStep.value;
+  const amount = refs.amount.value;
+
+  for (let i = 1; i < amount; i++) {
+    createPromise(amount, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
